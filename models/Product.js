88e6fb27efb3d1b929bbc521f518cf62b9d5b498
Cassie T <cassandray9@gmail.com>
2023-05-02 17:31:04 -0600

    Category, Product, ProductTag, Tag, index Models complete.

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const { INTEGER } = require('sequelize');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,6 +11,41 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
+      // Validates that the value is a decimal.
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      defaultValue: 10,
+      // Validates that the value is a number.
+      validate: {
+       isNumeric : true,
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+        unique: false,
+      }
+    }
   },
   {
     sequelize,
