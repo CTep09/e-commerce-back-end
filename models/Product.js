613392fa0d8c16bbf5bf734a0f8f4df9a606651e
Cassie T <cassandray9@gmail.com>
2023-05-02 20:45:37 -0600

    product-routes GET, POST, PUT complete

--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,6 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false, 
@@ -24,16 +23,16 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
+      // Validates that the value is a decimal
       validate: {
         isDecimal: true,
       }
-      // Validates that the value is a decimal.
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false, 
       defaultValue: 10,
-      // Validates that the value is a number.
+      // Validates that the value is a number
       validate: {
        isNumeric : true,
       }
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,31 +1,33 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: Product }]
-    })
+      include: [{ model: Product }],
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      include: [{ model: Product }]
-    })
+      include: [{ model: Product }],
+    });
 
     if (!categoryData) {
-      res.status(400).json({ message: "No category found with the provided id" });
+      res
+        .status(400)
+        .json({ message: "No category found with the provided id" });
 
       return;
     }
@@ -36,7 +38,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
   try {
     const categoryData = await Category.create(req.body);
@@ -46,17 +48,19 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
   try {
     const categoryData = await Category.update(req.body, {
       where: {
-        id: req.params.id
-      }
-    })
+        id: req.params.id,
+      },
+    });
 
     if (!categoryData[0]) {
-      res.status(400).json({ message: "No category found with the provided id" });
+      res
+        .status(400)
+        .json({ message: "No category found with the provided id" });
 
       return;
     }
@@ -67,17 +71,19 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
       where: {
-        id: req.params.id
-      }
-    })
+        id: req.params.id,
+      },
+    });
 
     if (!categoryData) {
-      res.status(400).json({ message: "No category found with the provided id" });
+      res
+        .status(400)
+        .json({ message: "No category found with the provided id" });
 
       return;
     }
