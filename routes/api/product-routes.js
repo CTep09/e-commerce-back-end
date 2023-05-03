13657fc9814def5457c7c3205f187dd383b960c1
Cassie T <cassandray9@gmail.com>
2023-05-03 09:25:23 -0600

    delete route complete for product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -114,6 +114,25 @@ router.put("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res
+        .status(400)
+        .json({ message: "No product found with the provided id" });
+
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
