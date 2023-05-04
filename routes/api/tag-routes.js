36a28dfa6155d08e7f983e4f15fa44e58d59f949
Cassie T <cassandray9@gmail.com>
2023-05-03 18:48:46 -0600

    Code cleanup

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -90,7 +90,7 @@ router.delete('/:id', async (req, res) => {
       return;
     }
 
-    res.status(200).json({message: "The tag has been updated"});
+    res.status(200).json({message: "The tag has been deleted"});
   } catch (err) {
     res.status(500).json(err);
   }
